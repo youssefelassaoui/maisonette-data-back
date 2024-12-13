fd236fcd8907d4de6368190fc94ca49d4ccea82f
youssef <youssef.elassaoui@gmail.com>
2024-12-13 10:08:30 +0100

    initial one

---
+++ src/main/java/gep/ma/maisonette_data_back/MaisonetteDataBackApplication.java
@@ -0,0 +1,13 @@
+package gep.ma.maisonette_data_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisonetteDataBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisonetteDataBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gep/ma/maisonette_data_back/MaisonetteDataBackApplicationTests.java
@@ -0,0 +1,13 @@
+package gep.ma.maisonette_data_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisonetteDataBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
